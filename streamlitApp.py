073706fe3413d0c55f57a60aa124fbac2f0e94cb
rakesh vishvakarma <rakesh.vishvakarma@rubik.ai>
2021-06-08 12:56:46 +0530

    small change in layout

--- streamlitApp.py
+++ streamlitApp.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import datetime as dt
+from datetime import date 
 
 import streamlit as st
 
@@ -7,35 +9,78 @@ st.title('The Modern Data Company')
 
 st.write("A small app!!")
 
+
+
+# Reading datasets
+
 consumers = pd.read_csv('SampleData/consumers.csv')
-timeseries = pd.read_csv('SampleData/sampleTimeSeries.csv')
+
+@st.cache(allow_output_mutation=True)
+def read_order_data():
+	d = pd.read_csv('SampleData/OrderData.csv')
+	return d
+
+order_data = read_order_data()
+
+order_data.rename(columns = {'Consumer_ID':'id'}, inplace = True)
+
+order_data.Order_DateTime = pd.to_datetime(order_data.Order_DateTime)
+
+order_data['date'] = order_data.Order_DateTime.dt.date
+
+
+order_data = order_data.merge(consumers[['id','type', 'state']], on = 'id', how = 'left')
+
+
+
+
+
 
 #order_data = pd.read_csv('SampleData/inlineItems.csv')
-type_of_consumer = consumers.groupby('type')['id'].size()
+type_of_consumer = consumers.groupby('type')['id'].size().T
+
+left_column, right_column = st.beta_columns(2)
 
 
-option = st.sidebar.selectbox(
+
+option1 = st.sidebar.selectbox(
     'Select State',
      ['All']+list(consumers.state.unique()))
 
-st.write('consumers by type')
+left_column.write('retailers by type')
 
-if option == 'All':
-	st.bar_chart(type_of_consumer)
+if option1 == 'All':
+	left_column.bar_chart(type_of_consumer, width=200, height=300)
 else:
-	st.bar_chart(consumers[consumers.state == option].groupby('type')['id'].size())
+	left_column.bar_chart(consumers[consumers.state == option1].groupby('type')['id'].size(), width=200, height=300)
 
 
 # Consumers Map Plot
-st.write("ConsumerMapPlot")
-if option == 'All':
-	st.map(consumers[['lon', 'lat']])
+right_column.write("retailers distribution across state/country")
+if option1 == 'All':
+	right_column.map(consumers[['lon', 'lat']], use_container_width=False)
 
 else:
-	st.map(consumers[consumers.state == option][['lon', 'lat']])
+	right_column.map(consumers[consumers.state == option1][['lon', 'lat']], use_container_width=False)
+
+
+# Option 2
+option2 = st.sidebar.selectbox(
+    'Select type',
+     ['All']+list(consumers.type.unique()))
 
 
 
-## line plot
-st.write('sampleTimeSeries')
-st.line_chart(timeseries)
+st.write(option1+" "+option2+' Sales')
+
+if option1 == 'All' and option2 == 'All':
+	st.line_chart(order_data.groupby('date')['Quantities'].sum())
+
+elif option1 == 'All' and option2 != 'All':
+	st.line_chart(order_data[order_data.type == option2].groupby('date')['Quantities'].sum())	
+
+elif option1 != 'All' and option2 == 'All':
+	st.line_chart(order_data[order_data.state == option1].groupby('date')['Quantities'].sum())
+
+else:
+	st.line_chart(order_data[(order_data.state == option1) & (order_data.type == option2)].groupby('date')['Quantities'].sum())
